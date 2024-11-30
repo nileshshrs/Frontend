610c783517c6dedddc621ecac1ced1d068c65e69
nileshshrs <nileshshr54@gmail.com>
2024-11-30 18:19:37 +0545

    fixed logout api request not working properly and ending the session

--- src/components/UserMenu.tsx
+++ src/components/UserMenu.tsx
@@ -3,10 +3,11 @@ import { useNavigate } from 'react-router-dom';
 import { logout } from '../api/api';
 import { queryClient } from '../main';
 import { Button } from './ui/button';
+import useAuth from '../hooks/useAuth';
 
 const UserMenu = () => {
     const navigate = useNavigate();
-
+    const { user, isLoading } = useAuth();
     const { mutate: signout } = useMutation({
         mutationFn: logout,
         onSuccess: () => {
@@ -15,35 +16,37 @@ const UserMenu = () => {
         },
     });
 
+    console.log(user)
     return (
-        <div className="h-full hidden lg:block w-full lg:max-w-[400px] py-10">
-            <div className="flex items-center justify-evenly">
-                <div className="w-full inline-flex items-center justify-start gap-5">
-                    <div className="">
-                        <img
-                            src="https://play-lh.googleusercontent.com/jInS55DYPnTZq8GpylyLmK2L2cDmUoahVacfN_Js_TsOkBEoizKmAl5-p8iFeLiNjtE=w526-h296-rw"
-                            alt=""
-                            width={"50px"}
-                            height={"50px"}
-                            className="rounded-full"
-                        />
+        isLoading ? <p>Loading...</p> :
+            <div className="h-full hidden lg:block w-full lg:max-w-[400px] py-10">
+                <div className="flex items-center justify-evenly">
+                    <div className="w-full inline-flex items-center justify-start gap-5">
+                        <div className="">
+                            <img
+                                src="https://play-lh.googleusercontent.com/jInS55DYPnTZq8GpylyLmK2L2cDmUoahVacfN_Js_TsOkBEoizKmAl5-p8iFeLiNjtE=w526-h296-rw"
+                                alt=""
+                                width={"50px"}
+                                height={"50px"}
+                                className="rounded-full"
+                            />
+                        </div>
+                        <div>
+                            <div>username</div>
+                            <div>email</div>
+                        </div>
                     </div>
-                    <div>
-                        <div>username</div>
-                        <div>email</div>
+                    <div className="w-full">
+                        <Button
+                            className="text-primary font-bold"
+                            variant={"ghost"}
+                            onClick={() => signout()} // Call signout function here
+                        >
+                            Logout
+                        </Button>
                     </div>
-                </div>
-                <div className="w-full">
-                    <Button
-                        className="text-primary font-bold"
-                        variant={"ghost"}
-                        onClick={() => signout()} // Call signout function here
-                    >
-                        Logout
-                    </Button>
                 </div>
             </div>
-        </div>
     );
 };
 
